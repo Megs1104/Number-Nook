df3dc0f57b2337a9624d20bf9dfc206b49b62a0f
Megan <megangilling1@icloud.com>
2025-06-17 10:30:53 +0100

    Initial commit.

--- src/features/home/components/Header.tsx
+++ src/features/home/components/Header.tsx
@@ -1,9 +1,7 @@
 export default function Header() {
   return (
     <div className="flex justify-center">
-      <p className="text-4xl text-white font-bold">
-        Mr Duncanson's Number Nook
-      </p>
+      <p className="text-4xl text-white font-bold">Number Nook</p>
     </div>
   );
 }

cb6aca02e17b7963e6a8bc58da1107cfe64a944c
Megan <megangilling1@icloud.com>
2025-07-16 16:41:59 +0100

    Resolving issues with drag and drop for touchscreen.

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,32 @@ import { Route, Routes } from "react-router-dom";
 import TensFrame from "./features/tens-frame/TensFrame";
 import NumberLineToTen from "./features/number-line-to-ten/NumberLineToTen";
 import NumberLineToTwenty from "./features/number-line-to-twenty/NumberLineToTwenty";
-
+import { DndProvider } from "react-dnd";
+import { TouchBackend } from "react-dnd-touch-backend";
 function App() {
   return (
-    <div className="relative min-h-screen w-screen">
-      <img
-        src={brownPaper}
-        alt="Brown Paper Overlay"
-        className="absolute w-full h-full object-cover pointer-events-none z-0"
-      />
-      <div className="relative z-10 flex flex-col min-h-screen w-full">
-        <div className="bg-darkGreen shadow-xl w-[85vw] max-w-[1200px] mx-auto min-h-screen">
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/tens-frame" element={<TensFrame />} />
-            <Route path="/number-line-to-ten" element={<NumberLineToTen />} />
-            <Route
-              path="/number-line-to-twenty"
-              element={<NumberLineToTwenty />}
-            />
-          </Routes>
+    <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+      <div className="relative min-h-screen w-screen">
+        <img
+          src={brownPaper}
+          alt="Brown Paper Overlay"
+          className="absolute w-full h-full object-cover pointer-events-none z-0"
+        />
+        <div className="relative z-10 flex flex-col min-h-screen w-full">
+          <div className="bg-darkGreen shadow-xl w-[85vw] max-w-[1200px] mx-auto min-h-screen">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/tens-frame" element={<TensFrame />} />
+              <Route path="/number-line-to-ten" element={<NumberLineToTen />} />
+              <Route
+                path="/number-line-to-twenty"
+                element={<NumberLineToTwenty />}
+              />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </DndProvider>
   );
 }
 

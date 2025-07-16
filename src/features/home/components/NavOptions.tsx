2199701e8639ac3c2dd52f9a5b4cdcc34052e119
Megan <megangilling1@icloud.com>
2025-07-16 15:53:10 +0100

    Adding number lines to 10 and 20 and improving inital navigation.

--- src/features/home/components/NavOptions.tsx
+++ src/features/home/components/NavOptions.tsx
@@ -1,19 +1,47 @@
 import { Link } from "react-router-dom";
 import tensFrame from "../../../assets/tens-frame.png";
+import numberLineToTen from "../../../assets/number-line-to-10.png";
+import numberLineToTwenty from "../../../assets/number-line-to-20.png";
 
 export default function NavOptions() {
   return (
-    <div className="flex flex-col text-center justify-center mt-5">
+    <div className="flex-1 flex-col text-center justify-center mt-5">
       <p className="mb-2 text-xl">Choose a manipulative</p>
-      <Link to="/tens-frame">
-        <button>
-          <img
-            src={tensFrame}
-            alt="Tens Frame Preview"
-            className="rounded w-64 object-contain"
-          />
-        </button>
-      </Link>
+
+      <div className="p-2">
+        <Link to="/tens-frame">
+          <button>
+            <img
+              src={tensFrame}
+              alt="Tens Frame Preview"
+              className="rounded w-100 object-contain"
+            />
+          </button>
+        </Link>
+      </div>
+      <div className="p-2">
+        <Link to="/number-line-to-ten">
+          <button>
+            <img
+              src={numberLineToTen}
+              alt="Number Line to 10 Preview"
+              className="rounded w-100 object-contain"
+            />
+          </button>
+        </Link>
+      </div>
+
+      <div className="p-2">
+        <Link to="/number-line-to-twenty">
+          <button>
+            <img
+              src={numberLineToTwenty}
+              alt="Number Line to 20 Preview"
+              className="rounded w-100 object-contain"
+            />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ src/features/number-line-to-ten/NumberLineToTen.tsx
@@ -0,0 +1,12 @@
+import NumberLine from "../home/components/number-line/NumberLine";
+import NavBar from "../navigation/NavBar";
+
+export default function NumberLineToTen() {
+  return (
+    <div>
+      <NavBar />
+      <p className="text-center text-3xl">Number Line to 10</p>
+      <NumberLine min={0} max={10} />
+    </div>
+  );
+}
